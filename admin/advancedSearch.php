806fb7bd11aa3ba705b7ba5a8c16cbb87da6b5ab
Sean Chen <sean.chen@leocorn.com>
2017-05-01 10:20:42 -0400

    create utility function to build the dashboard form

--- admin/advancedSearch.php
+++ admin/advancedSearch.php
@@ -16,14 +16,15 @@ if (isset($_POST['searchstrap_settings_form_submit']) &&
     $_POST['searchstrap_settings_form_submit'] == 'Y') {
 
     // handle form submit.
-    $action = $_POST['action'];
+    $submit_action = $_POST['submit-action'];
     $ssdb = new SearchstrapDb();
 
-    switch($action) {
+    switch($submit_action) {
     case $label_save_action:
         $id = $ssdb->replace_advanced_option(
             stripslashes($_POST['searchstrap_key']),
-            stripslashes($_POST['searchstrap_options']));
+            stripslashes($_POST['searchstrap_options']),
+            (int) $_POST['searchstrap_id']);
         $msg = "Setting Updated Successfully! $id";
         break;
     default:
@@ -56,62 +57,71 @@ if (isset($_POST['searchstrap_settings_form_submit']) &&
         }
     }
 }
-
-//$options = get_option('searchstrap_options');
-//if($options === false) {
-//    // no options set up yet, set the default.
-//    // This is a sample default filter options, based on the 
-//    // solr syntax.
-//    //$options = st_livesearch_default_options();
-//}
-//
-//$key = get_option('searchstrap_key');
-//if($input_id === false) {
-//    // the default input id will be livesearch.
-//    //$input_id = st_livesearch_default_input_id();
-//}
 ?>
 
 <div class="wrap">
   <h2>SearchStrap - Advanced Search Settings</h2>
   <p>Manage options for advances search.</p>
 
-  <form name="advancedsearch_settings_form" method="post">
-    <input type="hidden" name="searchstrap_settings_form_submit"
-           value="Y"/>
-    <table class="form-table"><tbody>
-      <tr>
-        <th scope="row">Advanced Search Key:</th>
-        <td>
-          <input name="searchstrap_key" size="80"
-                 value="<?php echo $param_key;?>"
-          >
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Advanded Search Options: <br/>
-        (One option each line, add comma at the end of the line)
-        </th>
-        <td>
-          <textarea name="searchstrap_options"
-                    rows="6" cols="98"
-          ><?php echo $current_option['wpss_option'];?></textarea>
-        </td>
-      </tr>
-      <tr>
-        <td></td>
-        <th scope="row">
-          <input type="submit" name="action" 
-                 class="button-primary" 
-                 value="<?php echo $label_save_action;?>" />
-        </th>
-      </tr>
-    </tbody></table>
-  </form>
-
   <?php
+    echo searchstrap_advancedsearch_settings_form($current_option);
     echo '<h3>Available Key Options</h3>';
     // show all active git repos in jQuery DataTables.
     echo searchstrap_build_options_list();
   ?>
 </div>
+
+<?php
+/**
+ * utility function to build the advanced search settings form.
+ */
+function searchstrap_advancedsearch_settings_form($current_option) {
+
+   if(!empty($current_option)) {
+       // load the current options.
+       $the_id = $current_option['wpss_id'];
+       $the_key = $current_option['wpss_key'];
+       $the_option = $current_option['wpss_option'];
+   } else {
+       // this will be the case to create new options.
+       $the_id = 0;
+   }
+
+   $form = <<<EOT
+<form name="advancedsearch_settings_form" method="post">
+  <input type="hidden" name="searchstrap_settings_form_submit"
+         value="Y"/>
+  <input type="hidden" name="searchstrap_id" value="{$the_id}"/>
+  <table class="form-table"><tbody>
+    <tr>
+      <th scope="row">Advanced Search Key:</th>
+      <td>
+        <input name="searchstrap_key" size="80"
+               value="{$the_key}"
+        >
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">Advanded Search Options: <br/>
+      (One option each line, add comma at the end of the line)
+      </th>
+      <td>
+        <textarea name="searchstrap_options"
+                  rows="6" cols="98"
+        >{$the_option}</textarea>
+      </td>
+    </tr>
+    <tr>
+      <td></td>
+      <th scope="row">
+        <input type="submit" name="submit-action" 
+               class="button-primary" 
+               value="Save Options" />
+      </th>
+    </tr>
+  </tbody></table>
+</form>
+EOT;
+
+    return $form;
+}
