0aa15fe18074349a6a0f35a6db738ac00a87bcae
Sean Chen <sean.chen@leocorn.com>
2017-05-10 10:49:43 -0400

    add the template for building user profiles list

--- resources/templates/defaultTemplates.js
+++ resources/templates/defaultTemplates.js
@@ -83,3 +83,90 @@ var acronymPanelStripper = function(acronym) {
         '</div>';
     return panel;
 };
+
+/**
+ * build the user profile list, which will have 6 columns
+ */
+var buildUserProfilesList = function(docs, currentQuery, total,
+                            currentPage, totalPages, pagination) {
+
+    var resultSummary = '';
+    if(total > 0) {
+        var end = currentQuery.start +
+                  currentQuery.perPage - 1;
+        end = end > total ? total : end;
+        resultSummary =
+            'Page <strong>' + currentPage + '</strong>' +
+            ' Showing [<strong>' + currentQuery.start +
+            '</strong> - <strong>' + end +
+            '</strong>] of <strong>' +
+            total + '</strong> total results';
+    } else {
+        // no result found
+        resultSummary =
+            '<strong>No results containing ' +
+            'all your search terms were found.</strong>';
+    }
+    jQuery('#search-info').html(resultSummary);
+
+    // build a 6 columns to show
+    var result = jQuery("#result-list");
+    result.html("");
+
+    //result.append('<div>' + pagination + '</div>');
+    var colQueue =[];
+    for(i = 0; i < docs.length; i++) {
+        var acronym = docs[i];
+        var panel = userProfilePanel(acronym);
+        colQueue.push(panel);
+        // i count from 0
+        // 6 acronyms for a row
+        var ready2Row = (i + 1) % 4;
+        if(ready2Row == 0) {
+            result.append('<div class="row">' +
+                colQueue.join("") + '</div>');
+            // reset the queue.
+            colQueue = [];
+        }
+    }
+
+    // check if we missed anything...
+    if(colQueue.length > 0) {
+
+        // append to the last row.
+        result.append('<div class="row">' +
+            colQueue.join(" ") +
+            '</div>');
+    }
+
+    // add the pagination at the bottom too.
+    result.append('<div>' + pagination + '</div>');
+
+    return result;
+};
+
+/**
+ * builder function to strip out all wiki syntax.
+ */
+var userProfilePanel = function(profile) {
+
+    // try to remove some wiki markups.
+    var desc = profile['content'];
+
+    var panel = '<div class="col-sm-3">' +
+'<div>' +
+'  <a class="pull-left" href="' + profile['url'] + '">' +
+'  <img src="' + profile['image'] +
+            '" class="img-circle" width=86 alt="' +
+            profile['title'] + '"/>' +
+'  </a>' +
+'  <div>' +
+'    <h4><a href="' + profile['url'] + '">' + profile['title'] +
+'</a></h4>' +
+'    <p>' + desc +'</p>' +
+'  </div>' +
+'</div>' +
+        '</div>';
+
+    return panel;
+};
